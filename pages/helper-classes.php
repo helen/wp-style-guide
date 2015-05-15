ff3141a52c27c2d9f065aa22e4487fe0b9824270
Helen Hou-Sandi <helen.y.hou@gmail.com>
2015-05-15 18:03:06 -0400

    Use post-3.8 helper classes and update some more CSS naming.

--- pages/helper-classes.php
+++ pages/helper-classes.php
@@ -3,69 +3,61 @@
 
 	<h2><?php esc_html_e( 'Color Helper Classes' ); ?></h2>
 
-	<div class="mp6-sg-example">
+	<div class="wp-pattern-example">
 		<h3>Blocks</h3>
 
-		<table class="mp6-table">
+		<table class="wp-pattern-table">
 		<thead>
 			<tr>
-				<th class="sg-example-code"><?php esc_html_e( 'Class' ); ?></th>
-				<th class="sg-example-descrip"><?php esc_html_e( 'Description' ); ?></th>
+				<th class="example-code"><?php esc_html_e( 'Class' ); ?></th>
+				<th class="example-descrip"><?php esc_html_e( 'Description' ); ?></th>
 			</tr>
 		</thead>
 		<tbody>
 			<tr>
-				<td><code>.mp6-primary</code></td>
-				<td><span class="mp6-primary"><?php esc_html_e( "Text with this class uses the base color." ); ?></span></td>
+				<td><code>.wp-ui-primary</code></td>
+				<td><span class="wp-ui-primary"><?php esc_html_e( "Elements with this class uses the base color." ); ?></span></td>
 			</tr>
 			<tr>
-				<td><code>.mp6-highlight</code></td>
-				<td><span class="mp6-highlight"><?php esc_html_e( "Text with this class uses the highlight color." ); ?></span></td>
+				<td><code>.wp-ui-highlight</code></td>
+				<td><span class="wp-ui-highlight"><?php esc_html_e( "Elements with this class uses the highlight color." ); ?></span></td>
 			</tr>
 			<tr>
-				<td><code>.mp6-notification</code></td>
-				<td><span class="mp6-notification"><?php esc_html_e( "Text with this class uses the notification color." ); ?></span></td>
+				<td><code>.wp-ui-notification</code></td>
+				<td><span class="wp-ui-notification"><?php esc_html_e( "Elements with this class uses the notification color." ); ?></span></td>
 			</tr>
 		</tbody>
 	</table>
 	</div>
 
-	<div class="mp6-sg-example">
+	<div class="wp-pattern-example">
 		<h3>Text</h3>
 
-		<table class="mp6-table">
+		<table class="wp-pattern-table">
 		<thead>
 			<tr>
-				<th class="sg-example-code"><?php esc_html_e( 'Class' ); ?></th>
-				<th class="sg-example-descrip"><?php esc_html_e( 'Description' ); ?></th>
+				<th class="wp-pattern-example-code"><?php esc_html_e( 'Class' ); ?></th>
+				<th class="wp-pattern-example-descrip"><?php esc_html_e( 'Description' ); ?></th>
 			</tr>
 		</thead>
 		<tbody>
 			<tr>
-				<td><code>.mp6-text-primary</code></td>
-				<td><span class="mp6-text-primary"><?php esc_html_e( "Text with this class uses the base color." ); ?></span></td>
+				<td><code>.wp-ui-text-primary</code></td>
+				<td><span class="wp-ui-text-primary"><?php esc_html_e( "Text with this class uses the base color." ); ?></span></td>
 			</tr>
 			<tr>
-				<td><code>.mp6-text-highlight</code></td>
-				<td><span class="mp6-text-highlight"><?php esc_html_e( "Text with this class uses the highlight color." ); ?></span></td>
+				<td><code>.wp-ui-text-highlight</code></td>
+				<td><span class="wp-ui-text-highlight"><?php esc_html_e( "Text with this class uses the highlight color." ); ?></span></td>
 			</tr>
 			<tr>
-				<td><code>.mp6-text-notification</code></td>
-				<td><span class="mp6-text-notification"><?php esc_html_e( "Text with this class uses the notification color." ); ?></span></td>
+				<td><code>.wp-ui-text-notification</code></td>
+				<td><span class="wp-ui-text-notification"><?php esc_html_e( "Text with this class uses the notification color." ); ?></span></td>
 			</tr>
 			<tr>
-				<td><code>.mp6-text-icon</code></td>
-				<td><span class="mp6-text-icon"><?php esc_html_e( "Text with this class uses the icon color." ); ?></span></td>
+				<td><code>.wp-ui-text-icon</code></td>
+				<td><span class="wp-ui-text-icon"><?php esc_html_e( "Text with this class uses the icon color." ); ?></span></td>
 			</tr>
 		</tbody>
 	</table>
 	</div>
-
-	<div class="mp6-sg-example">
-		<h3>Dashicons</h3>
-		<h4 class="icon-dashicon"><?php esc_html_e( "This heading has an icon." ); ?></h4>
-		<pre><code>&lt;h4 class="icon-dashicon"&gt;<?php esc_html_e( "This heading has an icon." ); ?>&lt;/h4&gt;</code></pre>
-		<p>Add an icon to things using <code>.icon-dashicon</code> and overwrite the <code>content</code> property with a value from the <a href="http://melchoyce.github.io/dashicons/">Dashicons reference page</a>.</p>
-	</div>
-
 </div><!-- .wrap -->
